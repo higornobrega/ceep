5e22aa75ba8a7edbb615502b826381523bc0982e
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-10-19 11:08:56 -0300

    Criando lista de categorias

---
+++ src/components/ListaDeCategoria/ListaDeCategoria.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class ListaDeCategoria extends Component {
+    render() { 
+        return ( 
+            <section>
+                <ul>
+                    <li>Categoria</li>
+                    <li>Categoria</li>
+                    <li>Categoria</li>
+                    <li>Categoria</li>
+                </ul>
+                <input type="text"/>
+            </section>
+         );
+    }
+}
+ 
+export default ListaDeCategoria;
