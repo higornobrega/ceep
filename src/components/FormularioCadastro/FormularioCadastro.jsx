4b7c229f1fcdb0b7138358a958f307655d329398
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-11-02 11:05:04 -0300

    Referências pra funções e bind

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -7,12 +7,20 @@ class FormularioCadastro extends Component {
     super(props);
     this.titulo ="";
     this.texto = "";
-    this.categoria = "";
+    this.categoria = "Sem Categoria";
     this.state = {categorias:[]}
+    
+    this._novasCategorias = this._novasCategorias.bind(this)
+
   }
 
   componentDidMount(){
-    this.props.categorias.inscrever(this._novasCategorias.bind(this));
+    this.props.categorias.inscrever(this._novasCategorias);
+  }
+
+  componentWillUnmount(){
+    this.props.categorias.desinscrever(this._novasCategorias);
+
   }
 
   _novasCategorias(categorias){
